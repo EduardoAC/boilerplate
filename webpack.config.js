4bbfcaea07139f5cc22a4e3cac1ce0892b86ad53
Eduardo Aparicio Cardenes <EduardoAC@users.noreply.github.com>
2018-07-28 15:42:20 +0200

    Add TypeScript Support

--- webpack.config.js
+++ webpack.config.js
@@ -3,10 +3,32 @@ const path = require('path');
 
 module.exports = {
   context: path.resolve(__dirname, 'src'),
-  entry: './index.js',
+  entry: './index.ts',
+  devtool: 'inline-source-map',
+  resolve: {
+    extensions: [ '.tsx', '.ts', '.js' ]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(tsx|ts)$/,
+        enforce: 'pre',
+        loader: 'tslint-loader',
+        options: { /* Loader options go here */ }
+      },
+      {
+        test: /\.(tsx|ts)?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
   },
-  plugins: [new HtmlWebpackPlugin()]
+  plugins: [new HtmlWebpackPlugin({
+    template: path.resolve(__dirname, 'src', 'assets', 'index.template.html')
+  })]
+
 };
