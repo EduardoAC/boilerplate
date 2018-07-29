b55edaf4773e2c122f3664379f75126c91e2a2fb
Eduardo Aparicio Cardenes <EduardoAC@users.noreply.github.com>
2018-07-29 12:49:45 +0200

    Add Relay and GraphQL support

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,10 @@
+const RelayCompilerWebpackPlugin = require('relay-compiler-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
+const sourceFiles = path.resolve(__dirname, 'src');
 module.exports = {
-  context: path.resolve(__dirname, 'src'),
+  context: sourceFiles,
   entry: './index.tsx',
   devtool: 'source-map',
   resolve: {
@@ -27,8 +29,14 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
   },
-  plugins: [new HtmlWebpackPlugin({
-    template: path.resolve(__dirname, 'src', 'assets', 'index.template.html')
-  })]
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'src', 'assets', 'index.template.html'),
+    }),
+    new RelayCompilerWebpackPlugin({
+      schema: path.resolve(__dirname, 'schema.graphql'), // or schema.json or a GraphQLSchema instance
+      src: sourceFiles,
+    })
+  ]
 
 };
